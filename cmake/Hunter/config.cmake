a6ab9babe917c029b71fc0ab41494a37d03965e8
Hussam <hussam@dominantstrategies.io>
2024-08-12 15:31:13 -0500

    Fork of RavenCommunity/kawpowminer @ 632f6ea
    This is a fork of the kawpowminer project, originally forked from ethminer: https://github.com/RavenCommunity/kawpowminer
    Forked at commit: https://github.com/RavenCommunity/kawpowminer/commit/632f6ea0a5cd09e2c6443374dbe6db0a767715ba

---
+++ cmake/Hunter/config.cmake
@@ -0,0 +1,7 @@
+hunter_config(CURL VERSION ${HUNTER_CURL_VERSION} CMAKE_ARGS HTTP_ONLY=ON CMAKE_USE_OPENSSL=ON CMAKE_USE_LIBSSH2=OFF CURL_CA_PATH=none)
+hunter_config(Boost VERSION 1.70.0-p0)
+
+hunter_config(ethash VERSION 1.0.0
+    URL https://github.com/RavenCommunity/cpp-kawpow/archive/1.1.0.tar.gz
+    SHA1 fff78f555a43900b6726c131305a71be769ef769
+)
