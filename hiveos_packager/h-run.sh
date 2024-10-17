a37e4e8a4c18dea387b3ebe6ff4334e47df91722
Hussam <hussam@dominantstrategies.io>
2024-10-17 13:38:33 -0500

    Fix issues with h-run.sh on Hive 20.04

--- hiveos_packager/h-run.sh
+++ hiveos_packager/h-run.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
-[[ `ps aux | grep "quai-gpu-miner" | grep -v grep | wc -l` != 0 ]] &&
+[[ `pgrep -f "quai-gpu-miner --api-bind" | wc -l` != 0 ]] &&
   echo -e "${RED}$CUSTOM_NAME miner is already running${NOCOLOR}" &&
   exit 1
 
 . h-manifest.conf
 
-
 conf=`cat $MINER_CONFIG_FILENAME`
 
-if [[ $conf=~';' ]]; then
-    conf=`echo $conf | tr -d '\'`
+if [[ $conf =~ ';' ]]; then
+    conf=`echo $conf | tr -d '\\'`
 fi
 
 eval "unbuffer ./quai-gpu-miner ${conf//;/'\;'} --api-bind 127.0.0.1:21373"
+
+sleep 10
