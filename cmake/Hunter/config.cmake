2ad5d9f3769a119634e21e85891b50760b57db09
alan <652732310@qq.com>
2024-10-31 05:54:16 +0800

    make it compile for ubuntu 22

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,5 +1,8 @@
 hunter_config(CURL VERSION ${HUNTER_CURL_VERSION} CMAKE_ARGS HTTP_ONLY=ON CMAKE_USE_OPENSSL=ON CMAKE_USE_LIBSSH2=OFF CURL_CA_PATH=none)
-hunter_config(Boost VERSION 1.70.0-p0)
+hunter_config(Boost VERSION 1.81.0
+    URL https://github.com/boostorg/boost/releases/download/boost-1.81.0/boost-1.81.0.tar.gz
+    SHA1 f71f661f893f39b7b82d66c54980349716f874a2
+)
 
 hunter_config(ethash VERSION 1.0.0
     URL https://github.com/RavenCommunity/cpp-kawpow/archive/1.1.0.tar.gz
